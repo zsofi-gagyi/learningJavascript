b82bd86e07af3fae2a53e1b2afc4cab9fc64bbee
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-12-06 04:13:26 +0100

    minor changes after an unsuccesful attempt to repair its packages

--- angular/rx-js-game/e2e/src/app.e2e-spec.ts
+++ angular/rx-js-game/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to rx-js-game!');
+    expect(page.getTitleText()).toEqual('Welcome to game!');
   });
 
   afterEach(async () => {
